ccc8d853c4fe672e64873de9728fb6d28c0a0e3c
Evan Lovely <evan.lovely@ombuweb.com>
2012-10-30 18:46:45 -0700

    Adding icons to toolbar

--- ombu_toolbar.tpl.php
+++ ombu_toolbar.tpl.php
@@ -7,17 +7,16 @@
 ?>
 <div class="navbar navbar-inverse navbar-fixed-top overlay-displace-top">
     <div class="navbar-inner">
-        <!-- <div class="container"> Needed when going to a responsive navbar -->
+        <div class="container">
             <a class="btn btn-navbar" data-toggle="collapse" data-targe=".nav-collapse">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </a>
-            <a href="/" class="brand">Menu</a>
             <div class="nav-collapse">
               <?php print render($menu) ?>
               <?php print render($drawer) ?>
             </div>
-        <!-- </div> -->
+        </div>
     </div>
 </div>
